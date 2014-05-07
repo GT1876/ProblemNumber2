22b23b5af77da99740853a39c10674de02cb6b5f
GT1876 <garrysontime@gmail.com>
2014-05-07 09:52:30 -0400

    yay

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static string Reversed(string input)
+        {
+            string output = "";
+
+            char[] seq = input.ToCharArray();
+
+            Array.Reverse(seq);
+
+            return output;
+        }
+
+
+        static void Main(string[] args)                                                                                                     
+        {
+            Console.Write("Enter a string: ");
+
+            string input = Console.ReadLine();
+            string output = Reversed(input);
+            string output = Reverse(Console.ReadLine);
+
+            Console.WriteLine(output);
+        }
+    }
+}
+
+
